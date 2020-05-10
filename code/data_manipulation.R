cba707061c31ae28e39a1363094e6cde3aee06b2
richardrex <rfrf26@mail.ru>
2020-05-10 16:41:14 +0200

    started eda project(end till 01/06)

---
+++ code/data_manipulation.R
@@ -0,0 +1,5 @@
+runoff_eu_day <- readRDS('C:/Users/42077/Documents/eda_eu_rivers/data/runoff_eu_day.rds')
+runoff_eu_info <- readRDS('C:/Users/42077/Documents/eda_eu_rivers/data/runoff_eu_info.rds')
+runoff_eu_year <- readRDS('C:/Users/42077/Documents/eda_eu_rivers/data/runoff_eu_year.rds')
+### need to make tidy data tables runoff per day with station names (mb countries)
+### try to continue in python
